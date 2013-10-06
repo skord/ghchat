f7b53d650cb6f5788b2e235f61d80fc3c4b6f9a6
Mike Danko <danko@giveforward.com>
2013-10-06 13:13:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ghchat::Application.config.secret_key_base = 'a9083ed72d411f3aeb1724bb85eccb26666003352779d6510a9eb06785787cff5bd0cb381ab5d97ce89419dd5534534fa7450c8c81c4ec75f025b719cb751978'
